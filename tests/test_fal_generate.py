206da6dee3a51722a7d2352c6a138a80888139d7
bparlan <bparlan@gmail.com>
2026-09-07 12:42:20 +0300

    auto-checkpoint: 2026-09-07 12:42:20

--- tests/test_fal_generate.py
+++ tests/test_fal_generate.py
@@ -3,7 +3,7 @@
 
 import sys
 import os
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from fal_generate import get_resolution, estimate_cost
 
